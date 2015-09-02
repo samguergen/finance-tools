21f55353b9b35ee792d5aef4367dfcb049818561
Samantha Guergenenov <samanthaguergenenov@Presents-MacBook-Pro.local>
2015-09-02 15:56:50 -0400

    referenced template url

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,11 +1,18 @@
-this.app = angular.module('app', ['templates', 'ngRoute', 'ui.slider','chart.js']);
+angular.module('app', ['templates', 'ngRoute', 'ui.slider','chart.js']).config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/mortgage', {
+                templateUrl: 'home.html',
+                controller: 'HomeCtrl'
+            });
+        $locationProvider.html5Mode(true);
+    });
 
-this.app.config([
-  '$httpProvider', function($httpProvider) {
-    return $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-  }
-]);
+// this.app.config([
+//   '$httpProvider', function($httpProvider) {
+//     return $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+//   }
+// ]);
 
-this.app.run(function() {
-  return console.log('angular app running');
-});
+// this.app.run(function() {
+//   return console.log('angular app running');
+// });
