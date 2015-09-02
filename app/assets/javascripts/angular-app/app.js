a9243df5492c2a3e25fd9617fefa60b4f5d03640
Samantha Guergenenov <samanthaguergenenov@Presents-MacBook-Pro.local>
2015-09-02 17:18:57 -0400

    loaded angular OK

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,18 +1,12 @@
-angular.module('app', ['templates', 'ngRoute', 'ui.slider','chart.js']).config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'home.html',
-                controller: 'HomeCtrl'
-            });
-        $locationProvider.html5Mode(true);
-    });
 
-// this.app.config([
-//   '$httpProvider', function($httpProvider) {
-//     return $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-//   }
-// ]);
+this.app = angular.module('app', ['ui.slider','chart.js']);
 
-// this.app.run(function() {
-//   return console.log('angular app running');
-// });
+this.app.config([
+  '$httpProvider', function($httpProvider) {
+    return $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  }
+]);
+
+this.app.run(function() {
+  return console.log('angular app running');
+});
