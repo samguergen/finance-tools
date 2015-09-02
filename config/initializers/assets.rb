44a1f96e2aef12be4bd08bb00332d01ba1ae1948
Samantha Guergenenov <samanthaguergenenov@Presents-MacBook-Pro.local>
2015-09-02 16:39:37 -0400

    removed non vendor css files from assets pipeline because already loaded

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( mortgage.css slider.css angular-chart.css slider.js Chart.js mortgage.js angular-chart.js pv-logo.png)
+Rails.application.config.assets.precompile += %w( slider.css angular-chart.css slider.js Chart.js angular-chart.js )
